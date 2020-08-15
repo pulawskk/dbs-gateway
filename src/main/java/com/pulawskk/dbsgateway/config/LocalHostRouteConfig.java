97acbddc0fd35685f665dfdbaa0b31d06df2462b
drogba <pulawskk@gmail.com>
2020-08-15 17:58:45 +0200

    Add routing for order microservice

---
+++ src/main/java/com/pulawskk/dbsgateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,22 @@
+package com.pulawskk.dbsgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/burgers/*", "/api/v1/burgers")
+                    .uri("http://localhost:8882")
+                    .id("dbs-food"))
+                .route(r -> r.path("/api/v1/orders")
+                    .uri("http://localhost:8884")
+                    .id("dbs-order"))
+                .build();
+    }
+}
