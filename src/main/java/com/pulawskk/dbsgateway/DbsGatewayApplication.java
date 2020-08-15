770e1a7e700fc2417ff3cd7ae578c830f5c82718
drogba <pulawskk@gmail.com>
2020-08-15 11:18:50 +0200

    Initial commit

---
+++ src/main/java/com/pulawskk/dbsgateway/DbsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pulawskk.dbsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbsGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pulawskk/dbsgateway/DbsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulawskk.dbsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
