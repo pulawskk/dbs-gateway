c86a85d7f0330d071dc97d330c9224845ad499fb
drogba <pulawskk@gmail.com>
2020-09-04 18:35:54 +0200

    Dockerize dbs-gateway service

--- src/main/java/com/pulawskk/dbsgateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/pulawskk/dbsgateway/config/LoadBalancedRoutesConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("local-discovery")
 public class LoadBalancedRoutesConfig {
 
     @Bean
--- src/main/java/com/pulawskk/dbsgateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/pulawskk/dbsgateway/config/LocalHostRouteConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local-discovery")
 @Configuration
 public class LocalHostRouteConfig {
 
